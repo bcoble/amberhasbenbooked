79a80185c86260f59009b34bd339695315e90e81
Ben Coble <ben.j.coble@gmail.com>
2019-11-04 21:13:14 -0600

    Remove creds

--- scripts/rsvp.js
+++ scripts/rsvp.js
@@ -1,28 +1,3 @@
 console.log('hook to google sheets');
 
-var $form = $('form#rsvp-form'),
-    url = 'https://script.google.com/macros/s/AKfycbwCTPFpbEd5dODjuqF2JUvRvhETNkj6w1AXrqlcICNkL8yz4hM/exec'
-
-$('#rsvp-submit-btn').on('click', function(e) {
-  e.preventDefault();
-  var jqxhr = $.ajax({
-    url: url,
-    method: "GET",
-    dataType: "json",
-    data: $form.serializeObject()
-  }).success(
-    // do something
-    console.log('api success');
-  );
-});
-
-
-
-
-
-$(document).on('change', '#rsvp-decline', function(val) {
-  console.log('change');
-});
-
-// DO NOT DELETE
-// https://script.google.com/macros/s/AKfycbwCTPFpbEd5dODjuqF2JUvRvhETNkj6w1AXrqlcICNkL8yz4hM/exec
+// remove google doc sync
