816eb42f772bc55d6890059406a5ae4a13f96b98
Ben Coble <ben.j.coble@gmail.com>
2019-11-04 21:09:32 -0600

    IT WORKS

--- scripts/rsvp.js
+++ scripts/rsvp.js
@@ -3,22 +3,19 @@ console.log('hook to google sheets');
 var $form = $('form#rsvp-form'),
     url = 'https://script.google.com/macros/s/AKfycbwCTPFpbEd5dODjuqF2JUvRvhETNkj6w1AXrqlcICNkL8yz4hM/exec'
 
-$('#rsvp-submit-btn').on('click', function(e) {
-  e.preventDefault();
+$(document).on('submit', 'form', function(event){
+  const _data = $(this).serializeArray();
+  console.log(_data);
+  event.preventDefault();
   var jqxhr = $.ajax({
     url: url,
     method: "GET",
     dataType: "json",
-    data: $form.serializeObject()
-  }).success(
-    // do something
+    data: _data
+  }).done(function() {
     console.log('api success');
-  );
+  });
 });
-
-
-
-
 
 $(document).on('change', '#rsvp-decline', function(val) {
   console.log('change');
